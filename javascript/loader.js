0d0bf1e91630423be79d711ba867a2520f21a77d
Divesh Adivarekar <100069544+diveshadivarekar@users.noreply.github.com>
2023-11-13 18:26:20 +0530

    removing old files

--- javascript/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  const loader = document.getElementById("loader");
-  const content = document.getElementById("content");
-
-  loader.style.display = "none";
-  content.style.display = "block";
-});
